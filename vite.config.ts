9e6f4e68a86dd8b1b5bc52d1b3dfc0f6b40e6103
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-13 16:15:25 +0200

    [TM] chore: add aliases to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
     },
   },
 })
