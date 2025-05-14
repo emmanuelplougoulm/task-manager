844a18bacc2d9f05bfe9e626456138a2263aa232
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-14 15:05:36 +0200

    [TM] feat: add filtering statuses

--- src/components/TaskTable/TaskTable.vue
+++ src/components/TaskTable/TaskTable.vue
@@ -9,6 +9,21 @@ import { useTaskStore } from '@stores/taskStore'
 const taskStore = useTaskStore()
 const { tasks, hasTask } = toRefs(taskStore)
 
+const filterStatus = ref(null)
+const statusOptions = [
+  { label: 'all', value: null },
+  { label: 'todo', value: 'todo' },
+  { label: 'pending', value: 'pending' },
+  { label: 'done', value: 'done' },
+]
+const filteredTasks = computed(() => {
+  let filtered = tasks.value
+  if (filterStatus.value) {
+    filtered = filtered.filter((task) => task.status === filterStatus.value)
+  }
+  return filtered
+  console.log('filtered', filtered.value)
+})
 </script>
 
 <template>
@@ -19,6 +34,23 @@ const { tasks, hasTask } = toRefs(taskStore)
 
   <div v-if="hasTask">Display task list</div>
   <div v-else>You have no task yet, create one</div>
+  <table v-if="hasTask">
+    <thead>
+      <tr>
+        <th>Titre</th>
+        <th>Description</th>
+        <th>Statut</th>
+        <th @click="toggleSort">
+          Date limite
+          <!-- <span v-if="sortDirection === 'asc'">▲</span>
+          <span v-else>▼</span> -->
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <div v-for="task in filteredTasks" :key="task.id">
+        <!-- Don't forget to add logic to display a message when filtered items are 0 -->
+
     <TaskRow
       :title="task.title"
       :description="task.description"
@@ -26,6 +58,8 @@ const { tasks, hasTask } = toRefs(taskStore)
       :dueDate="task.dueDate"
     />
   </div>
+    </tbody>
+  </table>
 </template>
 
 <style scoped></style>
