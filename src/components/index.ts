9e73d10fc4e26df41faaf98c9ecb6f62d036383f
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-16 21:16:00 +0200

    [TM] chore: updated components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,22 @@ import BaseModal from '@components/BaseModal/BaseModal.vue';
 import Board from '@components/Board/Board.vue';
 import EditModal from '@components/EditModal/EditModal.vue';
 import AddModal from '@components/AddModal/AddModal.vue';
-import NewTaskCard from '@components/NewTaskCard/NewTaskCard.vue';
 import TaskRow from '@components/TaskRow/TaskRow.vue';
-import TaskTable from '@components/TaskTable/TaskTable.vue';
+import ListView from '@components/ListView/ListView.vue';
 import Toast from '@components/Toast/Toast.vue';
+import Tabs from '@components/Tabs/Tabs.vue';
+import Button from '@components/Button/Button.vue';
+import HomeHeader from '@components/HomeHeader/HomeHeader.vue';
 
-export { BaseModal, Board, EditModal, NewTaskCard, TaskRow, TaskTable, Toast };
+export {
+  BaseModal,
+  Board,
+  EditModal,
+  TaskRow,
+  ListView,
+  Toast,
+  Tabs,
+  Button,
+  HomeHeader,
+  AddModal
+};
