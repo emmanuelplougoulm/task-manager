35020080421e2ce8b0b912fc78162e1ea6ac7d15
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-16 17:54:47 +0200

    [TM] chore: add new colors load them into Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,17 @@
-import type { Preview } from '@storybook/vue3'
+import type { Preview } from '@storybook/vue3';
+import '../src/assets/base.css';
+import '../src/assets/main.css';
+import '../src/assets/variables.css';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
+    }
+  }
 };
 
 export default preview;
