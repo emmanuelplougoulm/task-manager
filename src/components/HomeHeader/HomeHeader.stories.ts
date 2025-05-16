7fe64413fd0f0e39fa07497f2dbd3a269daab27d
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-16 20:35:18 +0200

    [TM] feat: extract home header and make it component

---
+++ src/components/HomeHeader/HomeHeader.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import HomeHeader from './HomeHeader.vue';
+
+const meta: Meta<typeof HomeHeader> = {
+  title: 'Components/HomeHeader',
+  component: HomeHeader,
+  tags: ['autodocs'],
+  argTypes: {
+    title: {
+      control: { type: 'text' },
+      defaultValue: 'Bienvenue sur MangoPay',
+      description: 'Titre affiché à côté du logo'
+    }
+  }
+};
+export default meta;
+
+type Story = StoryObj<typeof HomeHeader>;
+
+export const Default: Story = {
+  args: {
+    title: 'Bienvenue sur MangoPay'
+  }
+};
---
+++ src/components/HomeHeader/HomeHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Logo from '@assets/mangopay__logo.png';
+
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <header class="home__header">
+    <img :src="Logo" class="home__header__logo" />
+    <h1 class="home__header__title">
+      {{ title }}
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+.home__header {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  border-radius: 4px;
+}
+.home__header__logo {
+  height: 20px;
+}
+.home__header__title {
+  font-size: 24px;
+}
+</style>
