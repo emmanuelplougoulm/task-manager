91ca3e97f6d01e340005202a2d6da4b7b68c9154
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-18 12:35:48 +0200

    [TM] feat: display message when screen is to small

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,75 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { TaskTable, Board, Tabs } from '@components/index';
-import Logo from '@assets/mangopay__logo.png';
+import { ref, computed, toRefs } from 'vue';
+import { Table, Board, Header, Actions } from '@components/index';
+import { useTaskStore } from '@/stores';
+import { useViewportWidth } from '@/composable/useViewportWidth';
 
 const taskStore = useTaskStore();
-const { hasTask, filterStatus, filteredTasks, sortOrder } = toRefs(taskStore);
-];
+const { hasTask, filteredTasks } = toRefs(taskStore);
 
-const setActiveTab = (index: number): void => {
-  activeTab.value = index;
-};
+const activeTab = ref(0);
+const showTable = computed(() => activeTab.value === 0);
+
+const { width } = useViewportWidth();
+const isReadable = computed(() => width.value > 400);
 </script>
 
 <template>
+  <main class="home">
+    <Header title="Welcome to task master by" />
+    <div v-if="isReadable" class="home__content">
       <Actions v-model="activeTab" :showTable="showTable" />
       <Table v-if="showTable" :hasTask="hasTask" :filteredTasks="filteredTasks" />
+      <Board v-else />
+    </div>
+    <div class="home__warning_message" v-else>
+      <p>
+        Oh snap ! It looks like you're screen is very small<br />We recommend you switch
+        to desktop
+      </p>
     </div>
   </main>
 </template>
 
 <style scoped>
-main {
+* {
+  box-sizing: border-box;
+}
+
+.home {
   font-family: var(--font-family);
   display: flex;
   flex-direction: column;
+  gap: 30px;
   height: 100%;
-  border: 1px red solid;
 }
 
-header {
-  border: 1px violet solid;
-  height: 50px;
+.home__content {
+  display: flex;
+  flex-direction: column;
+  gap: 35px;
+  background-color: white;
+  border-radius: 8px;
+
+  padding: 10px;
+  height: 100%;
 }
 
-.content {
-  background-color: lightgrey;
+.home__warning_message {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+  background-color: var(--color-secondary-bg);
   height: 100%;
-  padding: 2rem;
+}
+
+.home__warning_message p {
+  text-align: center;
+  font-family: var(--font-family);
+  color: var(--color-primary);
+  font-size: 18px;
+  font-weight: 600;
 }
 </style>
