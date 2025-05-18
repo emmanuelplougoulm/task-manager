25435ef97406b624ed6829fec5697e1d32b4ef74
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-18 17:24:43 +0200

    [TM] refact: tidy up Board

--- src/components/Board/Board.vue
+++ src/components/Board/Board.vue
@@ -1,22 +1,23 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import List from './components/List.vue';
+import { BOARD_LISTS } from '@constants/index';
+</script>
+
 <template>
   <div class="board">
     <List v-for="(list, index) in BOARD_LISTS" :key="index" :listName="list" />
   </div>
 </template>
 
-<script setup lang="ts">
-import List from './components/List.vue';
-import { BOARD_LISTS } from '@constants/index';
-</script>
-
 <style scoped>
 .board {
-  background-color: transparent;
+  border-radius: 0.5rem;
   height: 100%;
-  padding: 2rem;
+  padding: 16px;
   display: flex;
   align-items: flex-start;
-  flex-wrap: nowrap;
-  overflow-x: auto;
+  gap: 0.625rem;
+  overflow-x: scroll;
 }
 </style>
