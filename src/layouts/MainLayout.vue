e69463c6c1ff8ab5fe0761ea469d1e98fe83ead6
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-22 18:36:42 +0200

    [TM] refact: add /page + /layouts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<!-- src/layouts/MainLayout.vue -->
+<template>
+  <div class="main-layout">
+    <Navbar />
+    <Sidebar />
+    <main>
+      <slot />
+      <!-- use it to place your page content  -->
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+// import Navbar from '@/components/Navbar.vue'
+// import Sidebar from '@/components/Sidebar.vue'
+// import Footer from '@/components/Footer.vue'
+</script>
---
+++ src/pages/ProjectPage.vue
@@ -0,0 +1,12 @@
+<!-- this is an example -->
+<template>
+  <!-- <ProjectHeader :project="project" />
+  <SprintBoard :sprints="sprints" />
+  <KanbanBoard :tasks="tasks" /> -->
+</template>
+
+<script setup lang="ts">
+// import ProjectHeader from '@/modules/project/components/ProjectHeader.vue'
+// import SprintBoard from '@/modules/sprint/components/SprintBoard.vue'
+// import KanbanBoard from '@/modules/board/components/KanbanBoard.vue'
+</script>
