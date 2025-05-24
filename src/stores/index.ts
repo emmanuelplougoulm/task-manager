40bee25d0d4a73a193be29c692fa01aa9016511c
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-24 12:49:31 +0200

    [TM] chore: rm non transversal stores from root level folder

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,6 +1,4 @@
 import { useModalStore } from '@stores/modalStore';
-import { useNavigationStore } from '@stores/navigationStore';
-import { useTaskStore } from '@stores/taskStore';
 import { useToastStore } from '@/modules/notification/store/toastStore';
 
-export { useModalStore, useTaskStore, useToastStore, useNavigationStore };
+export { useModalStore, useToastStore };
