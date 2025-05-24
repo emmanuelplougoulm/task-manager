db6f21ca7cc60fa968ae176fdca272b5a7bebc5e
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-24 12:53:21 +0200

    [TM] refact: move table related components into Project module

---
+++ src/modules/project/components/TableBoard/TableCell.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+defineProps({
+  value: {
+    type: [String, Number]
+  }
+});
+</script>
+
+<template>
+  <td>{{ value }}</td>
+</template>
+
+<style>
+td {
+  padding: 4px 8px;
+  text-transform: capitalize;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-weight: 500;
+  font-size: var(--font-size-body);
+  color: var(--color-primary);
+}
+</style>
